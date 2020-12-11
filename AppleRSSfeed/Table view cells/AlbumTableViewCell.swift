02465b00db1c4e6fca1052ee7592f5ad5e718113
Anish <kode.iosdev@gmail.com>
2020-12-11 07:12:50 -0600

    albums table view cell config.

---
+++ AppleRSSfeed/Table view cells/AlbumTableViewCell.swift
@@ -0,0 +1,113 @@
+//
+//  AlbumTableViewCell.swift
+//  AppleRSSfeed
+//
+//  Created by Anish on 12/11/20.
+//  Copyright © 2020 Anish Kodeboyina. All rights reserved.
+//
+
+import UIKit
+
+class AlbumTableViewCell: UITableViewCell {
+    static let identifier = "albumTableViewCell"
+    
+    var viewModel: AlbumCellInformation?
+    
+    private var albumImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        
+        imageView.layer.cornerRadius = 8
+        imageView.layer.masksToBounds = true
+        
+        return imageView
+    }()
+    
+    private var imageViewContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private var nameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.adjustsFontForContentSizeCategory = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.lineBreakMode = .byWordWrapping
+        label.font = .preferredFont(forTextStyle: UIFont.TextStyle.headline, compatibleWith: UIScreen.main.traitCollection)
+        return label
+    }()
+    
+    private var artistLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.adjustsFontForContentSizeCategory = true
+        label.lineBreakMode = .byWordWrapping
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: UIFont.TextStyle.subheadline, compatibleWith: UIScreen.main.traitCollection)
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(imageViewContainer)
+        addSubview(nameLabel)
+        addSubview(artistLabel)
+        
+        imageViewContainer.addSubview(albumImageView)
+        self.accessoryType = .disclosureIndicator
+        
+        albumImageView.backgroundColor = UIColor.red
+        
+        NSLayoutConstraint.activate([
+            imageViewContainer.topAnchor.constraint(equalTo: self.topAnchor),
+            imageViewContainer.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            imageViewContainer.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            
+            albumImageView.topAnchor.constraint(equalTo: imageViewContainer.topAnchor, constant: 10),
+            albumImageView.leadingAnchor.constraint(equalTo: imageViewContainer.leadingAnchor, constant: 10),
+            albumImageView.trailingAnchor.constraint(equalTo: imageViewContainer.trailingAnchor, constant: -10),
+            albumImageView.bottomAnchor.constraint(greaterThanOrEqualTo: imageViewContainer.bottomAnchor, constant: -10),
+            
+            nameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: imageViewContainer.trailingAnchor, constant: 12),
+            nameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            
+            artistLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 12),
+            artistLabel.leadingAnchor.constraint(equalTo: imageViewContainer.trailingAnchor, constant: 12),
+            artistLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8),
+            artistLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            
+            albumImageView.widthAnchor.constraint(equalToConstant: 80)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        guard let viewModel = viewModel else { return }
+        
+        artistLabel.text = viewModel.artist
+        nameLabel.text = viewModel.name
+        //albumImageView.image = UIImage.init viewModel.albumImage
+    }
+}
+
+struct AlbumCellInformation {
+    var name: String
+    var artist: String
+    var albumImage: String
+}
+
+class AlbumTableViewCellViewModel {
+    var albumInformation: AlbumCellInformation?
+    
+    init(albumInfo: AlbumCellInformation) {
+        albumInformation = albumInfo
+    }
+}
--- AppleRSSfeed/View Controllers/AlbumsViewController.swift
+++ AppleRSSfeed/View Controllers/AlbumsViewController.swift
@@ -67,6 +67,9 @@ extension AlbumsViewController: UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: AlbumTableViewCell.identifier) as? AlbumTableViewCell else {
             return emptyCell
         }
+        cell.viewModel = AlbumCellInformation(name: "Name of Album",
+                                              artist: "Name of Artist",
+                                              albumImage: "Album Image")
         cell.configure()
         return cell
     }
@@ -89,89 +92,7 @@ extension AlbumsViewController: UITableViewDelegate {
 
 class AlbumsViewModel {
     var viewTitle = "Albums"
-}
-
-class AlbumTableViewCell: UITableViewCell {
-    static let identifier = "albumTableViewCell"
     
-    private var albumImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFit
-        
-        imageView.layer.cornerRadius = 8
-        imageView.layer.masksToBounds = true
-        
-        return imageView
-    }()
-    
-    private var imageViewContainer: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    private var nameLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.adjustsFontForContentSizeCategory = true
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.lineBreakMode = .byWordWrapping
-        return label
-    }()
-    
-    private var artistLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.adjustsFontForContentSizeCategory = true
-        label.lineBreakMode = .byWordWrapping
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        addSubview(imageViewContainer)
-        addSubview(nameLabel)
-        addSubview(artistLabel)
-        
-        imageViewContainer.addSubview(albumImageView)
-        self.accessoryType = .disclosureIndicator
-        
-        albumImageView.backgroundColor = UIColor.red
-        
-        NSLayoutConstraint.activate([
-            imageViewContainer.topAnchor.constraint(equalTo: self.topAnchor),
-            imageViewContainer.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            imageViewContainer.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            
-            albumImageView.topAnchor.constraint(equalTo: imageViewContainer.topAnchor, constant: 10),
-            albumImageView.leadingAnchor.constraint(equalTo: imageViewContainer.leadingAnchor, constant: 10),
-            albumImageView.trailingAnchor.constraint(equalTo: imageViewContainer.trailingAnchor, constant: -10),
-            albumImageView.bottomAnchor.constraint(greaterThanOrEqualTo: imageViewContainer.bottomAnchor, constant: -10),
-            
-            nameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 8),
-            nameLabel.leadingAnchor.constraint(equalTo: imageViewContainer.trailingAnchor, constant: 12),
-            nameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            
-            artistLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 12),
-            artistLabel.leadingAnchor.constraint(equalTo: imageViewContainer.trailingAnchor, constant: 12),
-            artistLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8),
-            artistLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            
-            albumImageView.widthAnchor.constraint(equalToConstant: 80)
-        ])
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func configure() {
-        artistLabel.text = "Hello"
-        nameLabel.text = "Name"
-    }
     
 }
 
