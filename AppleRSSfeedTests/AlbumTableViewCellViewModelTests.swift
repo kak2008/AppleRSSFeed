8a3a9654e4d4f0b25ab3b1a1bc220c265e0da18a
Anish <kode.iosdev@gmail.com>
2020-12-14 06:05:25 -0600

    test cases modifications.

--- AppleRSSfeedTests/AlbumTableViewCellViewModelTests.swift
+++ AppleRSSfeedTests/AlbumTableViewCellViewModelTests.swift
@@ -17,7 +17,6 @@ class AlbumTableViewCellViewModelTests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        
         viewModel = AlbumTableViewCellViewModel(mockAlbumInfo)
     }
     
