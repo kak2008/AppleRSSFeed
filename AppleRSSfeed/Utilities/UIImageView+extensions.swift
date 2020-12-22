81b340cc9c7de315440cbfec927235121eed7a17
Anish <kode.iosdev@gmail.com>
2020-12-21 23:10:27 -0500

    added cache functionality for image view.

--- AppleRSSfeed/Utilities/UIImageView+extensions.swift
+++ AppleRSSfeed/Utilities/UIImageView+extensions.swift
@@ -11,13 +11,26 @@ import UIKit
 
 extension UIImageView {
     func load(url: URL) {
+        let cache = URLCache.shared
+        let request = URLRequest(url: url)
+        
         DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url) {
-                if let image = UIImage(data: data) {
-                    DispatchQueue.main.async {
-                        self?.image = image
+            
+            if let data = cache.cachedResponse(for: request)?.data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self?.image = image
+                }
+            } else {
+                let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                    if let data = data, let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode), let image = UIImage(data: data) {
+                        let cacheData = CachedURLResponse(response: response, data: data)
+                        cache.storeCachedResponse(cacheData, for: request)
+                        DispatchQueue.main.async {
+                            self?.image = image
+                        }
                     }
                 }
+                task.resume()
             }
         }
     }
